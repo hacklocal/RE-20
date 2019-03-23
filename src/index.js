e50f7e6f14250448f7285a445e22c4f05c9e8f80
Riccardo <rispoliriccardo@gmail.com>
2019-03-23 19:03:51 +0100

    login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./containers/App"
-import Title from "./containers/Title"
-import Evento from "./containers/Event"
+import App from "./containers/App.js"
+import Login from "./containers/Login.js"
+import Title from "./containers/Title.js"
+import Event from "./containers/Event.js"
 
-import "./styles/base.min.css"
+import "./styles/base.css"
 
 import { Route, BrowserRouter as Router } from "react-router-dom"
 import { createBrowserHistory } from 'history'
@@ -14,7 +15,8 @@ const history = createBrowserHistory()
 ReactDOM.render(
   <Router history={history}>
     <Title />
-    <Route exact path="/" component={ App } />
-    <Route path="/event/:eventName" render={(props) => <Evento {...props} />}/>
+    <Route exact path = { "/" } component = { App } />
+    <Route exact path = { "/login" } component = { Login }/>
+    <Route path="/event/:eventName" render={(props) => <Event {...props} />}/>
   </Router>
   , document.getElementById('root'));
