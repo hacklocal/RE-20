c261bbe5268ac53661f34ada166b073871ecade2
Riccardo <rispoliriccardo@gmail.com>
2019-03-24 02:35:34 +0100

    event mods

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react"
 import {Map} from "google-maps-react";
 import logo from "../assets/logo.png"
+import { chunk } from "../helpers/utils.js"
+import { getEvent, getEventPartecipants } from "../helpers/api.js"
+import { Row, Col } from "react-bootstrap"
 class Event extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      description: "",
+      image: "",
+      partecipants: []
+    }
+  }
+
+  componentDidMount() {
+    getEvent(1).then(({ name, description, image }) => this.setState({ name, description, image }))
+    getEventPartecipants(1).then(partecipants => this.setState({ partecipants }))
+  }
+
   render() {
+    console.log(this.state.partecipants)
     return (
       <div id = { "event" }>
-        <img src = { logo }/>
-        <h1>Titolo</h1>
-        <textarea readOnly id = { "description" }>Ciao</textarea>
+        <div id = { "image-container" }><img src = { this.state.image } id = { "image" }/></div>
+        <h1>{ this.state.name }</h1>
+        <textarea readOnly id = { "description" } value = { this.state.description }></textarea>
         <fieldset>
           <legend>
             <span className = { "number" }>1</span> Scegli come contribuire:
           </legend>
-          <input type = "checkbox"/>
+          <div className="inputGroup">
+            <input id="option1" name="option1" type="checkbox"/>
+            <label htmlFor="option1">Option One</label>
+          </div>
+          <div className="inputGroup">
+            <input id="option2" name="option2" type="checkbox"/>
+            <label htmlFor="option2">Option Two</label>
+          </div>
         </fieldset>
         <fieldset>
           <legend>
             <span className = { "number" }>2</span> Partecipanti:
           </legend>
         </fieldset>
+        {
+          this.state.partecipants.map(partecipant => (
+            <img src = { partecipant.name }/>
+          ))
+        }
         {/*
         <Map
             google = { this.props.google }
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,3 +1,11 @@
-export const login = (username, password) => fetch("192.168.43.212:8000/api/authentication")
+/*export const login = (username, password) => fetch("192.168.43.212:8000/api/authentication")
+  .then(res => res.json())
+  .catch(console.log)*/
+
+export const getEvent = id => fetch(`http://192.168.43.212:8000/api/events/${id}`)
+  .then(res => res.json())
+  .catch(console.log)
+
+export const getEventPartecipants = id => fetch(`http://192.168.43.212:8000/api/events/${id}/users`)
   .then(res => res.json())
   .catch(console.log)
