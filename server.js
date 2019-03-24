773173b26a9036db6c016d384a9e5043cd5a3553
Matteo Bonacini <matteo.bonacini@soluzionifutura.it>
2019-03-24 02:02:51 +0100

    added cors

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+
+  next()
+
+})
+
+app.options("*", () => {
+  res.send(200)
+})
+
+app.use((req, res, next) => {
   res.error = (error, statusCode = 500) => {
     res.status(statusCode).json({
       error
