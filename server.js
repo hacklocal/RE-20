a7fdfa06f033faa28f225bf096face09febd0144
Matteo Bonacini <matteo.bonacini@soluzionifutura.it>
2019-03-24 04:19:13 +0100

    boh cors

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 
   next()
-
 })
 
 app.options("*", (req, res) => {
-  res.send(200)
+  res.status(200).json({message: "ok"})
 })
 
 app.use((req, res, next) => {
