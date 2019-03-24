49d8c875abcb714dd73e2f105182bc5ddbb97755
Riccardo <rispoliriccardo@gmail.com>
2019-03-24 01:05:23 +0100

    mods event to pull

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react"
-import { Link } from 'react-router-dom'
-
+import {Map} from "google-maps-react";
+import logo from "../assets/logo.png"
 class Event extends Component {
   render() {
     return (
-      <div>
-      <Link to="/">Home</Link>
-      <h1>ID: { this.props.match.params.eventName }</h1>
+      <div id = { "event" }>
+        <img src = { logo }/>
+        <h1>Titolo</h1>
+        <textarea readOnly id = { "description" }>Ciao</textarea>
+        <fieldset>
+          <legend>
+            <span className = { "number" }>1</span> Scegli come contribuire:
+          </legend>
+          <input type = "checkbox"/>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <span className = { "number" }>2</span> Partecipanti:
+          </legend>
+        </fieldset>
+        {/*
+        <Map
+            google = { this.props.google }
+            style = {{ width: "400px", height: "400px", borderRadius: "50%" }}
+            zoom = { 15 }
+            minZoom = { 11 }
+            initialCenter = {{
+              lat,
+              lng
+            }}
+        />*/}
       </div>
     )
   }
